29c0cb0a76697bbd4dcce8173bdbde5dd801fab6
Shahid <shahididrishi998@gmail.com>
2026-06-26 21:20:17 +0530

    avatar fixing

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,11 @@ app.use(cors({
   origin: '*',
   credentials: true
 }));
-
 app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  // res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
-
 app.use(express.json({ limit: '10mb' }));
 
 const publicPath = path.join(__dirname, '..', 'public');
